38232b0e4b02be7535ce7fd4aab75f816056995a
ParthRKhakre <parthrkhakre@gmail.com>
2026-05-22 11:04:53 +0530

    Fast API

--- 4 - FAST API/2 - FASTAPI/main.py
+++ 4 - FAST API/2 - FASTAPI/main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+import json
 
 app = FastAPI()
 
+def load_data():  
+    with open('patients.json','r') as f:
+# Above line of code loads the data from a json file in read only mode  
+        data = json.load(f)
+# json.load(f)convert JSON file data into a python dictionary/list. 
+    return data    
+
 @app.get("/")
 def hello():
-    return {'message':'Hello World'}
+    return {"message":"Patient Management API"}
 
-@app.GET('/about')
+@app.get("/about")
 def about():
-    return {'message':'Campusx'}
+    return {"message" : "A fully functional API to manage your patient records"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+    
