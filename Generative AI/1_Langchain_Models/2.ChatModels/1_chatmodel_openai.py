302e2e48e1045221c942d8e196e308949c70624b
ParthRKhakre <parthrkhakre@gmail.com>
2026-07-09 23:55:12 +0530

    changes

--- Generative AI/1_Langchain_Models/2.ChatModels/1_chatmodel_openai.py
+++ Generative AI/1_Langchain_Models/2.ChatModels/1_chatmodel_openai.py
@@ -11,6 +11,12 @@ model = ChatOpenAI(model="gpt-4",temperature=2,max_completion_tokens=200)
 # Higher Values : 0.7 to 1.5
 # max_completion_tokens means what maximum tokens AI can use
 
+# Temperature if kept = 0 then you will get the same output every time 
+# as we change the value to 0.5 the output from llm changes a little 
+# and soon the output generated is creative.
+# Hence when you want you application to respond the same manner for a 
+# defined input then keep temperature  = 0
+
 result = model.invoke("What is the capital of india")
 print(result.content)
 
--- Generative AI/2_Langchain_Prompt/temperature.py
+++ Generative AI/2_Langchain_Prompt/temperature.py
@@ -5,10 +5,20 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    temperature=0.7
+    temperature=0
     )
 
-result = model.invoke("Who is Mark Zuckerberg ?")
 
-print(result.content)
+result = model.invoke("Write a 5 line poem on cricket ?")
+
+# Query input message inside the invoke function is known as prompt 
+# Prompt are of two types : 
+# Text based prompt
+# Multimodel prompt (text + image/file/audio/video,etc)
+
+# LLM heavily depend on the prompt 
+# There are a lot ot methods to create prompt  
+# 1.Static prompt
+# 2.Dynamic Prompt 
+
 
